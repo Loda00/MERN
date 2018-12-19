4f900ee7849a1842476cb0b52603fb4db29057c1
Loda00 <jhon.1620@live.com>
2018-12-18 22:27:15 -0500

    config de react

---
+++ src/app/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+export class App extends Component {
+
+
+    render(){
+        return (
+            <h1> Javascript Fullstack Loda</h1>
+        )
+    }
+}
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
 const path = require('path')
 
 module.exports = {
-    entry: './src/app/app.js',
+    entry: './src/app/index.js',
     output:  {
-        path : path.join(__dirname + 'public')
+        path: path.join(__dirname + '/src/public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
     }
 }
